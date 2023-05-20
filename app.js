46e956700204bb1b3607a934ee7e5f6cd6a7bce1
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2023-05-20 13:13:32 -0700

    Module 1 Assignment

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 import express from 'express'
 
-// const path = require('path')
 
-// const express = require('express')
 
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')) //get the route and have it load 'index.html' from the public folder
+    res.sendFile(path.join(__dirname, 'public', 'index.html')) 
 })
-// app.get('/', (req, res) => {
-//     res.send("hello world")
-// })
+
 
 app.listen(5000,() => {
     console.log('listening to port 5000')
